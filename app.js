c8b4a37b1308f835620b6ca9ea14d1d947bb65b3
May <mayrampinto.@gmail.com>
2025-08-07 11:33:45 -0300

    fc agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function limpiarCaja() {
+  document.querySelector("#amigo").value = "";
+}
+
+function agregarAmigo() {
+  let nuevoAmigo = document.getElementById("amigo").value;
+  if (nuevoAmigo == "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    nombresAmigos.push(nuevoAmigo);
+    limpiarCaja();
+    console.log(nombresAmigos);
+  }
+}
